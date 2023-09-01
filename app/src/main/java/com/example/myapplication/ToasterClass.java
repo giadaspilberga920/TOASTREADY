bea521869d2361276750ec86e5a91d9f9be80f62
giadaspilberga920 <143806157+giadaspilberga920@users.noreply.github.com>
2023-09-01 18:15:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ToasterClass.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ToasterClass extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ToasterMessage.
+    }
+}
